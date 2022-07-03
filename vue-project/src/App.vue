0e50acafd104e57378860f0f7c1d57ac63081ca1
unknown <gm.c.burzo@gmail.com>
2022-07-03 09:46:32 -0400

    Alpha1

---
+++ vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+HELLO
+<Panels :questions="questions"/>
+</template>
+
+<script>
+import Panels from './components/Panels.vue';
+import data from './questions.json';
+
+export default{
+    data(){
+      return{
+        questions: data
+      }
+    },
+    components: {
+      Panels
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ vue-project/src/components/Panel.vue
@@ -0,0 +1,27 @@
+<template>
+<div class = "Panel">
+    <div>
+        <p>{{question.question}}</p>
+        <Question :choices = "question.choices" :questionsArray = "questionsArray"/>
+    </div>
+</div>
+</template>
+
+<script>
+import Question from './Question.vue';
+
+export default{
+    
+    name: 'Panel',
+    props: {
+        question: Object,
+        questionsArray: Array
+    },
+    components: {
+        Question
+    }
+}
+</script>
+
+<style scope>
+</style>
